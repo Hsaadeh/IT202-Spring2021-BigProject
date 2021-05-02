4fd8f5e289b6101f7673a4a242fea3c5e13138f1
Hsaadeh <hsaadeh622@gmail.com>
2021-05-01 23:20:09 -0500

    Worked on map, weather api, park data api

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // top app bar instantiated
 let topAppBarElement = document.querySelector('.mdc-top-app-bar');
 
+
+let initParkCoords = (parkEndpoint) => {
+    let parks = [];
+    let parksXY = [];
+    fetch(parkEndpoint)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            data.forEach((entry) => {
+                if (!parks.includes(entry["park"])) {
+                    parks.push(entry["park"]);
+                    parksXY.push(entry["the_geom"]["coordinates"]);
+                }
+            })
+            return parksXY;
+        })
+}
+
 // map functionality //
 let map;
 
@@ -9,6 +29,49 @@ function initMap() {
         center: { lat: 41.8781, lng: -87.6298 },
         zoom: 8,
     });
+    // newMarker({ lat: 41.8781, lng: -87.6298 }); // test marker
+    //newMarker({ lat: 41.2781, lng: -87.6298 }); // test marker
+
+    //console.log(parksXY1[0][0]);
+    // TODO: init parkxy here
+    // let parks = [];
+    // let parksXY = [];
+    // fetch(parkEndpoint)
+    //     .then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((data) => {
+    //         // console.log(data);
+    //         data.forEach((entry) => {
+    //             if (!parks.includes(entry["park"])) {
+    //                 parks.push(entry["park"]);
+    //                 parksXY.push(entry["the_geom"]["coordinates"]);
+    //                 coords = entry["the_geom"]["coordinates"];
+    //                 lat = coords[1];
+    //                 lng = coords[0];
+    //                 console.log(lat);
+    //                 newMarker({lat: lat, lng: lng});
+    //             }
+    //         })
+    //     })
+
+    // parksXY.forEach((coordinate) =>{
+    //     let lat = coordinate[1];
+    //     let lng = coordinate[0];
+    //     console.log(lat);
+    //     console.log(lng);
+    //     newMarker({lat: lat, lng: lng});
+    // })
+}
+
+// newMarker function for Map
+let newMarker = (coordinates) => {
+    let mapMarker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        // passive: true
+        // title: title,
+    })
 }
 
 // buttons initialized //
@@ -70,46 +133,83 @@ filterParkButton.addEventListener("click", (event) => {
         .then((data) => {
             // work with data
             data.forEach((entry) => {
-                console.log(park);
+                let lat = entry["the_geom"]["coordinates"][1];
+                let lng = entry["the_geom"]["coordinates"][0];
+                newMarker({ lat: lat, lng: lng });
                 // createCard(entry);
             });
         });
 })
 
+plotAllButton.addEventListener("click", (event) => {
+    let parks = [];
+    let parksXY = [];
+    fetch(parkEndpoint)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            data.forEach((entry) => {
+                if (!parks.includes(entry["park"])) {
+                    parks.push(entry["park"]);
+                    parksXY.push(entry["the_geom"]["coordinates"]);
+                    coords = entry["the_geom"]["coordinates"];
+                    lat = coords[1];
+                    lng = coords[0];
+                    // console.log(lat);
+                    newMarker({ lat: lat, lng: lng });
+                }
+            })
+        })
+
+    // parksXY.forEach((coordinate) =>{
+    //     let lat = coordinate[1];
+    //     let lng = coordinate[0];
+    //     console.log(lat);
+    //     console.log(lng);
+    //     newMarker({lat: lat, lng: lng});
+    // })
+})
+
 // OpenWeather Api //
-//let weatherEndpoint = "api.openweathermap.org"; // this needs the correct endpoint with the api key
-//let weatherData_url = weatherEndpoint;
+let weatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=6251120bd87aacc80e3fcdce5920f9ee";
+let weatherData_url = weatherEndpoint;
+fetch(weatherData_url)
+    .then((response) => {
+        // console.log(response.json());
+        return response.json();
+    })
+    .then((data) => {
+        // console.log(data);
+        let temperature = data["main"]["temp"];
+        let temperatureF = (parseFloat(temperature) - 273.15) * 9/5 + 32;
+        document.getElementById("temperature").innerHTML = temperatureF.toFixed(2) + "°F";
+        // console.log(temperature);
+    })
 
 // City of Chicago Parks Api //
 let parkEndpoint = "https://data.cityofchicago.org/resource/eix4-gf83.json";
 let parkData_url = parkEndpoint;
 let parks = [];
-parks.sort();
-// let data = fetch(parkData_url);
-// console.log(data.json());
-
-// fetch(parkData_url)
-//         .then((response) => {
-//             let data = response.json();
-//             // let park = data[0];
-//             console.log(data);
-
-//             // console.log( data.json() );
-//         })
-
+let parksXY = [];
+// parks.sort();
 
 // creates a dropdown menu using park names
 fetch(parkData_url)
     .then((response) => {
-        return response.json()
+        return response.json();
     })
     .then((data) => {
+        // console.log(data);
 
         data.forEach((entry) => {
             if (!parks.includes(entry["park"])) {
                 parks.push(entry["park"]);
+                parksXY.push(entry["the_geom"]["coordinates"]);
             }
         });
+
         let dropdown_park_options_array = [];
         dropdown_park_options_array.push("<option value='' id='park' selected></option>")
         parks.forEach((element) => {
@@ -118,10 +218,8 @@ fetch(parkData_url)
         document.getElementById("dropdown_park_options").innerHTML = dropdown_park_options_array.join();
     });
 
-
-console.log(parks);
-
-
+// console.log(parks);
+// console.log(parksXY);
 // let menu = document.querySelector(".mdc-menu mdc-menu-surface");
 
 // Geolocation data - get the users current location //
